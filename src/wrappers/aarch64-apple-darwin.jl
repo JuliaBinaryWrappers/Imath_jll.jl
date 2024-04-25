d68760624b4cec94cb082eba2b14dea5f3f9a9f1
jlbuild <juliabuildbot@gmail.com>
2024-04-25 17:41:44 +0000

    Imath_jll build 3.1.11+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,12 +2,12 @@
 export libImath
 
 JLLWrappers.@generate_wrapper_header("Imath")
-JLLWrappers.@declare_library_product(libImath, "@rpath/libImath-3_1.30.dylib")
+JLLWrappers.@declare_library_product(libImath, "@rpath/libImath-3_1.29.dylib")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libImath,
-        "lib/libImath-3_1.30.0.1.dylib",
+        "lib/libImath-3_1.29.10.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
--- src/wrappers/i686-linux-gnu.jl
+++ src/wrappers/i686-linux-gnu.jl
@@ -2,7 +2,7 @@
 export libImath
 
 JLLWrappers.@generate_wrapper_header("Imath")
-JLLWrappers.@declare_library_product(libImath, "libImath-3_1.so.30")
+JLLWrappers.@declare_library_product(libImath, "libImath-3_1.so.29")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
